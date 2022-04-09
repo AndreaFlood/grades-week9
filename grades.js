fb96fd3fee24d62543adbcad7fe06ce2889ad38b
Andrea Flood <andreaflood@hotmail.com>
2022-04-10 00:48:10 +0100

    Initial commit

---
+++ grades.js
@@ -0,0 +1,10 @@
+function grade(){
+        var subjects= prompt ("enter number of subjects?");
+        console.log(subjects);
+
+        while(subjects>0){
+                subjects=subjects-1;
+               grades= prompt("enter grade for subject");
+                console.log(grades);   
+         }
+}
